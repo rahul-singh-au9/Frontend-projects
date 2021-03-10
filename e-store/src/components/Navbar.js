ebc8d6fe6cda8d731e160e2b9837675432f7d382
unknown <rahulsg1508@gmail.com>
2021-03-11 00:26:27 +0530

    E-store

--- e-store/src/components/Navbar.js
+++ e-store/src/components/Navbar.js
@@ -14,9 +14,9 @@ export default class Navbar extends Component {
 
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-             <Link to="/" className="nav-link">
-              products
-             </Link>
+              <Link to="/" className="nav-link">
+                E-Store
+              </Link>
           </li>
         </ul>
 
